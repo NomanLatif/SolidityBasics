11f2b7a3dca7cd98948e17bcbc3a256e3678b751
Noman Latif <nomanllatif@gmail.com>
2020-12-10 22:15:09 +0100

    Solidity201: Add another basic test

--- SolidityCourse201/test/helloworldtest.js
+++ SolidityCourse201/test/helloworldtest.js
@@ -7,4 +7,13 @@ contract("Helloworld", async function(){
     assert(message === "Hello Again!"
     , "Message should be Hello Again!");
   });
+
+  it("should set the message correclty", async function(){
+    let instance = await Helloworld.deployed();
+    let newMessage = "Testing Message";
+    await instance.setMessage(newMessage);
+    let message = await instance.getMessage();
+    assert(message === newMessage
+    , "Message should be " + newMessage + " But it was " + message);
+  });
 });
